fd10c6d8c8f23440ca1034b517ad4700f5c10eea
Juan José Melero <juanjose.melero@alten.es>
2022-09-21 09:14:07 +0200

    Lo de la cancelación.

--- app/src/main/java/meleros/paw/corrutinas/OtherActivity.kt
+++ app/src/main/java/meleros/paw/corrutinas/OtherActivity.kt
@@ -10,10 +10,17 @@ class OtherActivity : BaseActivity() {
     override fun getLayout() = R.layout.activity_other
 
     override fun getBaseViewModel(): BaseViewModel =
-        otherViewModel ?: ViewModelProvider(this)[OtherViewModel::class.java].also { otherViewModel = it }
+        otherViewModel
+            ?: ViewModelProvider(this)[OtherViewModel::class.java].also { otherViewModel = it }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        otherViewModel?.usarUnHilo()
+//        otherViewModel?.usarUnHilo()
+        otherViewModel?.cooperarAntesDeCadaProcesoCostoso(
+            listOf("1", "3", "5")
+        )
+        otherViewModel?.librosLiveData?.observe(this) {
+            textView?.text = it.joinToString("\n") { it.titulo }
+        }
     }
 }
--- app/src/main/java/meleros/paw/corrutinas/OtherViewModel.kt
+++ app/src/main/java/meleros/paw/corrutinas/OtherViewModel.kt
@@ -1,8 +1,21 @@
 package meleros.paw.corrutinas
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
 
 class OtherViewModel : BaseViewModel() {
+
+    val librosLiveData: MutableLiveData<List<LibroVO>> = MutableLiveData()
+    val biblioteca = listOf(
+        LibroDTO("Moby Dick", 1),
+        LibroDTO("Cuento de Navidad", 2),
+        LibroDTO("Teología aplicada al paganismo", 3),
+        LibroDTO("Sopa de ganso para el fracasado", 4),
+        LibroDTO("La sirenita", 5),
+    )
 
     fun usarUnHilo() {
         thread {
@@ -13,4 +26,43 @@ class OtherViewModel : BaseViewModel() {
             printWithTag("He terminado")
         }
     }
+
+    fun cooperarAntesDeCadaProcesoCostoso(ids: List<String>) {
+        viewModelScope.launch(Dispatchers.Default) {
+            val idLibros = ids.mapNotNull(String::toIntOrNull)
+            printAndPost("Mapeo de identificadores finalizado...")
+
+            val dtos = getLibros(idLibros)
+            printAndPost("Libros obtenidos...")
+
+            val bos = dtos.map { libro -> libro.toBo() }
+            printAndPost("Mapeado a BO finalizado...")
+
+            val vos = bos.map { libro -> libro.toVo() }
+            printAndPost("Mapeado a VO finalizado...")
+
+            librosLiveData.postValue(vos)
+        }
+    }
+
+    fun getLibros(idLibros: List<Int>): List<LibroDTO> {
+        Thread.sleep(3000L)
+        return idLibros.mapNotNull { id -> biblioteca.find { it.id == id } }
+    }
+
+    class LibroDTO(val titulo: String, val id: Int)
+
+    fun LibroDTO.toBo(): LibroBO {
+        Thread.sleep(1000L)
+        return LibroBO(titulo)
+    }
+
+    class LibroBO(val titulo: String)
+
+    fun LibroBO.toVo(): LibroVO {
+        Thread.sleep(1000L)
+        return LibroVO(titulo)
+    }
+
+    class LibroVO(val titulo: String)
 }
